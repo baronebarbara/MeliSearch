e834de96c50e544020cf73f70a736caa3515cf5b
babarone22 <barbara_barone2@hotmail.com>
2024-05-08 21:51:25 -0300

    adiciona algumas cores

--- MeliSearch/AppConfiguration/SceneDelegate.swift
+++ MeliSearch/AppConfiguration/SceneDelegate.swift
@@ -7,7 +7,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            let navigationController = UINavigationController(rootViewController: ViewController())
+            let navigationController = UINavigationController(rootViewController: HomeViewController())
             window.rootViewController = navigationController
             self.window = window
             window.makeKeyAndVisible()
