b82bb77c37a382e1562d86a2c06f0e2ec3d5d633
babarone22 <barbara_barone2@hotmail.com>
2024-05-08 22:24:05 -0300

    adiciona primeiros componentes da tela

--- MeliSearch/Scene/Home/HomeViewController.swift
+++ MeliSearch/Scene/Home/HomeViewController.swift
@@ -1,9 +1,65 @@
 import UIKit
 
-class HomeViewController: UIViewController {
-
+final class HomeViewController: UIViewController {
+    private lazy var searchView: UISearchBar = {
+        let search = UISearchBar()
+        search.translatesAutoresizingMaskIntoConstraints = false
+        search.layer.cornerRadius = 16
+        search.placeholder = "Procure por seus produtos"
+        return search
+    }()
+    
+    private lazy var mainStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 16
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private lazy var productsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Conheça seus produtos disponíveis:"
+        label.textColor = UIColor(named: "clr_background")
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        buildLayout()
+    }
+}
+
+
+extension HomeViewController: ViewConfiguration {
+    func configViews() {
+        view.backgroundColor = .white
+    }
+    
+    func buildViewHierarchy() {
+        view.addSubview(searchView)
+        view.addSubview(mainStack)
+        mainStack.addArrangedSubview(productsLabel)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            searchView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            searchView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            searchView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            mainStack.topAnchor.constraint(equalTo: searchView.bottomAnchor, constant: 24),
+            mainStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
+            mainStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+        ])
     }
 }
