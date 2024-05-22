4cfe302bc0633ccd26df1820a8809cb78a154dc5
Bárbara Barone <babi.barone@cora.com.br>
2024-05-22 09:44:20 -0300

    configura spy

--- MeliSearchTests/Scenes/HomeInteractorSpec.swift
+++ MeliSearchTests/Scenes/HomeInteractorSpec.swift
@@ -1,7 +1,50 @@
 import XCTest
 
+@testable import MeliSearch
+
+private final class HomePresenterSpy: HomePresenterProtocol {
+    enum Method {
+        case presentProductSearch,
+             presentSelectedItem,
+             presentLoading,
+             presentEmptyState,
+             presentError,
+             presentInitialState
+    }
+    
+    private var methodsCalled: [Method] = []
+    private(set) var produtcSearch: ProductSearch?
+    private(set) var selectedItem: ProductSearchDetail?
+    private(set) var shouldPresentLoading: Bool?
+    
+    func present(productSearch: ProductSearch) {
+        self.produtcSearch = productSearch
+        methodsCalled.append(.presentProductSearch)
+    }
+    
+    func present(selectedItem: ProductSearchDetail) {
+        self.selectedItem = selectedItem
+        methodsCalled.append(.presentSelectedItem)
+    }
+    
+    func presentLoading(shouldPresent: Bool) {
+        self.shouldPresentLoading = shouldPresent
+        methodsCalled.append(.presentLoading)
+    }
+    
+    func presentEmptyState() {
+        methodsCalled.append(.presentEmptyState)
+    }
+    
+    func presentError() {
+        methodsCalled.append(.presentError)
+    }
+    
+    func presentInitialState() {
+        methodsCalled.append(.presentInitialState)
+    }
+}
+
 final class HomeInteractorSpec: XCTestCase {
-
-   
-
+    
 }
